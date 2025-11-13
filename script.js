12a913a4d52c6404fd2b5a413bcd1e8585820196
Cecilia <126418068+cecidiaz21@users.noreply.github.com>
2025-11-13 00:06:24 -0300

    cambio index y script

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
       // Cambia el color del fondo cuando el mouse pasa por encima
-      const boxHover = document.getElementById('boxHover');
-      boxHover.addEventListener('mouseover', function() {
-        boxHover.style.backgroundColor = '#ffd700';
-      });
-      boxHover.addEventListener('mouseout', function() {
-        boxHover.style.backgroundColor = '';
+      const boxHovers = document.querySelectorAll('.boxHover');
+      boxHovers.forEach(function(boxHover) {
+        boxHover.addEventListener('mouseover', function() {
+          boxHover.style.backgroundColor = '#ffd700';
+        });
+        boxHover.addEventListener('mouseout', function() {
+          boxHover.style.backgroundColor = '';
+        });
       });
     
